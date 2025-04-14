a622f0de039a3b77e44c9c2e4a4cb0c0f984facc
mirkarosko <miroslava.rosko@gmail.com>
2025-04-14 10:16:50 +0200

    vytvoreni utils.py

--- classes_dictionary.py
+++ classes_dictionary.py
@@ -1,6 +1,20 @@
 class Kniha:
     def __init__(self, id: int, name: str, author: str, year_publication: int, category: str, review: float,
                  borrow: bool) -> None:
+        if not isinstance(id, int):
+            raise TypeError("ID must be an integer")
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+        if not isinstance(author, str):
+            raise TypeError("Author must be a string")
+        if not isinstance(year_publication, int):
+            raise TypeError("Year publication must be an integer")
+        if not isinstance(category, str):
+            raise TypeError("Category must be a string")
+        if not isinstance(review, float):
+            raise TypeError("Review must be a float")
+
+
         self.id = id
         self.name = name
         self.author = author
--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from classes_dictionary import Kniha
 kniha = Kniha(1, "Voda", "Neznámý", 2020, "crimi", 4.5, False)
 print(kniha.name+"\nAuthor: "+kniha.author)
 
+try:
+    kniha = Kniha("text", "1984", "George Orwell", 2009, "novel", 4.6, True)
+except TypeError as chyba:
+    print(f'Chyba pri zadani: {chyba}')
