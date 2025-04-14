874a86b04c1afd73f679d398e10dcbcb98a70488
mirkarosko <miroslava.rosko@gmail.com>
2025-04-14 12:52:37 +0200

    class User, check_login, Users = []

--- classes_dictionary.py
+++ classes_dictionary.py
@@ -1,5 +1,20 @@
-class Kniha:
-    def __init__(self, id: int, name: str, author: str, year_publication: int, category: str, review: float,
+class Author:
+    def __init__(self, id: int, first_name: str, last_name: str, country: str) -> None:
+        if not isinstance(id, int):
+            raise TypeError("ID must be an integer")
+        if not isinstance(first_name, str):
+            raise TypeError("First name must be a string")
+        if not isinstance(last_name, str):
+            raise TypeError("Last name must be a string")
+        if not isinstance(country, str):
+            raise TypeError("Country must be a string")
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.country = country
+
+class Book:
+    def __init__(self, id: int, name: str, author: Author, year_publication: int, category: str, review: float,
                  borrow: bool) -> None:
         if not isinstance(id, int):
             raise TypeError("ID must be an integer")
@@ -30,5 +45,51 @@ class Kniha:
         if self.borrow:
             self.borrow = False
             print(f"Kniha {self.name} is free")
+
+class User:
+    def __init__(self, id: int, name: str, email: str, password: str) -> None:
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.borrowed_books = []
+
+    def borrow_book(self, book:Book):
+        if not book.borrow:
+            book.borrow = True
+            self.borrowed_books.append(book)
+            print(f"Book {self.name} borrowed")
+
+    def return_book(self, book:Book):
+            if book in self.borrowed_books:
+                book.borrow = False
+                self.borrowed_books.remove(book)
+                print(f"{self.name} returned book: {book.name}")
+            else:
+                print(f"{self.name} has not borrowed: {book.name}.")
+
+user1 = User(1, "Jana Novakova", "jana.novakova@email.cz", "heslo123")
+user2 = User(2, "Petr Maly", "petr.maly@email.cz", "tajne456")
+user3 = User(3, "Eva Velka", "eva.velka@email.cz", "mojeheslo789")
+user4 = User(4, "Tomas Horak", "tomas.horak@email.cz", "abc123")
+user5 = User(5, "Lucie Dvorakova", "lucie.d@email.cz", "lucinka456")
+user6 = User(6, "Martin Kral", "martin.kral@email.cz", "kral2024")
+user7 = User(7, "Barbora Kucerova", "bara.kucerova@email.cz", "kniha123")
+user8 = User(8, "Ondrej Nemec", "ondrej.n@email.cz", "tajneheslo")
+user9 = User(9, "Veronika Vesela", "verca.vesela@email.cz", "verca2024")
+user10 = User(10, "Michal Urban", "michal.urban@email.cz", "urban123")
+user11 = User(11, "Alena Cerna", "alena.cerna@email.cz", "cernalena")
+user12 = User(12, "Filip Prochazka", "filip.prochazka@email.cz", "filip123")
+user13 = User(13, "Helena Machova", "helena.m@email.cz", "machova2023")
+user14 = User(14, "David Novotny", "david.novotny@email.cz", "novotnyheslo")
+user15 = User(15, "Klara Ruzickova", "klara.ruzickova@email.cz", "ruzicka789")
+
+users = [
+    user1, user2, user3, user4, user5,
+    user6, user7, user8, user9, user10,
+    user11, user12, user13, user14, user15
+]
+
+
 
 
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from classes_dictionary import Kniha
+from classes_dictionary import Book, Author, User
+from classes_dictionary import users
 
 
-kniha = Kniha(1, "Voda", "Neznámý", 2020, "crimi", 4.5, False)
-print(kniha.name+"\nAuthor: "+kniha.author)
+book = Book(1, "Marco", "Adler Olsen", 2020, "crimi", 4.5, False)
+print(book.name+"\nAuthor: "+book.author)
 
 try:
-    kniha = Kniha("text", "1984", "George Orwell", 2009, "novel", 4.6, True)
+    book = Book("text", "1984", "George Orwell", 2009, "novel", 4.6, True)
 except TypeError as chyba:
     print(f'Chyba pri zadani: {chyba}')
+
+def check_login (write_email, write_password, users):
+    user_found = False
+    for user in users:
+        if user.email == write_email and user.password == write_password:
+            user_found = True
+            print("Login.")
+            return
+    print("Invalid email or password")
+
+write_email = input("Write your e-mail: ")
+write_password = input("Write your password: ")
+
+check_login(write_email, write_password, users)
+
+
